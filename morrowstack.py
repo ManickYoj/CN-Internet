2fa078b3778ba807b5183bcb9e3718e4fea017e7
unknown <nickfrancisci@gmail.com>
2014-04-26 17:48:27 -0400

    Continued work on morrowstack

--- morrowstack.py
+++ morrowstack.py
@@ -1,19 +1,39 @@
 class BaseLayer(object):
 
     def __init__(self, payload=None, verbose=False):
+        self.payload = None
+        self.verbose = verbose
+
         if isinstance(payload, BaseLayer) or payload is None:
             self.payload = payload
-            self.verbose = verbose
 
-            if self.verbose:
-                print("BaseLayer initilized with payload {}.").format(payload)
+    def getNext(self):
+        return self.payload
 
 
 class DatalinkLayer(BaseLayer):
 
-    def __init__(self, payload=None, verbose=False):
+    def __init__(self, mac, payload=None, verbose=False):
         super(DatalinkLayer, self).__init__(payload, verbose)
+        self.src_mac = mac[0]
+        self.dest_mac = mac[1]
+
+        if self.verbose:
+            print("DatalinkLayer initilized with a source MAC '{}'," +
+                  " a dest MAC '{}', and a payload '{}'").format(self.src_mac,
+                                                                 self.dest_mac,
+                                                                 self.payload)
+
+
+class IPLayer(BaseLayer):
+
+    def __init__(self, ip_data, payload, verbose=False):
+        super(IPLayer, self).__init__(payload, verbose)
+        self.src_ip = ip_data[0]
+        self.dest_ip = ip_data[1]
+        self.transport_protocol = ip_data[2]
+
 
 if __name__ == "__main__":
     print("Running unit tests for {}").format(__file__)
-    dl = DatalinkLayer(None, True)
+    dl = DatalinkLayer("IN", verbose=True)
