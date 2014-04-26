9d2c2fa9600e855f17ac6f422f6036e039fa3a1a
unknown <nickfrancisci@gmail.com>
2014-04-26 16:46:37 -0400

    Continued work on morrowstack

--- morrowstack.py
+++ morrowstack.py
@@ -1,4 +1,19 @@
-class BaseMorrowLayer:
-	
+class BaseLayer(object):
 
-class DatalinkLayer:
+    def __init__(self, payload=None, verbose=False):
+        if isinstance(payload, BaseLayer) or payload is None:
+            self.payload = payload
+            self.verbose = verbose
+
+            if self.verbose:
+                print("BaseLayer initilized with payload {}.").format(payload)
+
+
+class DatalinkLayer(BaseLayer):
+
+    def __init__(self, payload=None, verbose=False):
+        super(DatalinkLayer, self).__init__(payload, verbose)
+
+if __name__ == "__main__":
+    print("Running unit tests for {}").format(__file__)
+    dl = DatalinkLayer(None, True)
