134c2ebe530e74bc3d0f3b4c6562f76bb2300af2
unknown <nickfrancisci@gmail.com>
2014-04-27 03:21:31 -0400

    Preliminary completion and successful testing of the morrowstack

--- morrowstack.py
+++ morrowstack.py
@@ -2,7 +2,7 @@
 morrowstack.py
 --------------
 Author: Nick Francisci
-Status: Incomplete & Untested
+Status: Complete & Basic Functionality Tested
 Description:
 Contains the classes describing all stack layers.
 
@@ -17,6 +17,9 @@ Example Interfacing:
         2. In sockets layer: Initilize IP(Transport, (src_ip, dest_ip))
         3. In NIC layer: Initilize Datalink(IP, (src_mac, dest_mac))
         4. In NIC layer: Send Datalink.toBinary()
+
+
+TODO: Ensure that length/port parameters are parsed to int format correctly
 """
 
 
@@ -29,16 +32,16 @@ class BaseLayer(object):
     """
 
     def __init__(self, payload=None, header=None, verbose=False):
-        self.setPayload(payload)
-        self.setHeader(header)
         self.verbose = verbose
+        self.setPayload(payload, True)
+        self.setHeader(header, True)
 
     # ----- Public Methods ----- #
     def getPayload(self):
         """ Getter method for payload. """
         return self.payload
 
-    def setPayload(self, payload):
+    def setPayload(self, payload, init=False):
         """ Setter method with error-checking for payload. """
         self.checkPayload(payload)
         self.payload = payload
@@ -47,7 +50,7 @@ class BaseLayer(object):
         """ Getter method for header. """
         return self.header
 
-    def setHeader(self, header):
+    def setHeader(self, header, init=False):
         """ Setter method with error-checking for header. """
         self.checkHeader(header)
         self.header = header
@@ -55,13 +58,15 @@ class BaseLayer(object):
     # ----- Private Methods ----- #
     def checkPayload(self, payload):
         """ Checks to ensure that the payload meets format specifications. """
-        if not (isinstance(payload, BaseLayer) or isinstance(payload, basestring)):
-            raise ValueError("Error in the format of the payload of {}.").format(__name__)
+        if not (isinstance(payload, BaseLayer)):
+            exception_text = "Error in the format of the payload of {}.".format(self)
+            raise ValueError(exception_text)
 
     def checkHeader(self, header):
         """ Checks to ensure that the header meets format specifications. """
         if not (isinstance(header[0], basestring) and isinstance(header[1], basestring)):
-            raise ValueError("Error in the format of the header of {}.").format(__name__)
+            exception_text = "Error in the format of the header of {}.".format(self)
+            raise ValueError(exception_text)
 
 
 class DatalinkLayer(BaseLayer):
@@ -92,7 +97,7 @@ class IPLayer(BaseLayer):
         # Bottom up (msg recieved) initilization
         elif isinstance(payload, basestring):
             header = (payload[:2], payload[2:4])
-            self.transport_protocol = payload[4]
+            self.setTransportProtocol(payload[4])
             if self.transport_protocol == 'E':
                 payload = UDPLayer(payload[5:], verbose=verbose)
 
@@ -106,6 +111,22 @@ class IPLayer(BaseLayer):
                                                self.transport_protocol,
                                                self.payload)
 
+    # ----- Public Methods ----- #
+    def getTransportProtocol(self):
+        """ Getter method for transport_protocol attribute. """
+        return self.transport_protocol
+
+    def setTransportProtocol(self, transport_protocol):
+        """ Setter method for transport_protocol with error-checking. """
+        self.checkTransportProtocol(transport_protocol)
+        self.transport_protocol = transport_protocol
+
+    # ----- Private Methods ----- #
+    def checkTransportProtocol(self, transport_protocol):
+        """ Checks to ensure that the transport_protocol attribute matches format requirements. """
+        if not(isinstance(transport_protocol, basestring) and len(transport_protocol)) == 1:
+            raise ValueError("Invalid transport protocol value entered.")
+
 
 class TransportLayer(BaseLayer):
     """ Stand in for base class of UDPLayer and (theoretically) TCPLayer. """
@@ -114,6 +135,7 @@ class TransportLayer(BaseLayer):
 
 
 class UDPLayer(TransportLayer):
+    """ Morse implementation of UDP Transport Layer. """
     def __init__(self, payload=None, header=None, verbose=False):
         # Top down (msg constructed) initilization
         if payload and header:
@@ -121,8 +143,8 @@ class UDPLayer(TransportLayer):
 
         # Bottom up (msg recieved) initilization
         elif payload:
-            header = (payload[0], payload[1])
-            self.length = int(payload[2:4])
+            header = (payload[0], payload[1])  # TODO: Base36 Parse this
+            self.length = int(payload[2:4])  # TODO: ASCII Parse this, PS, this gets overwritten automatically
             payload = payload[4:]
 
         super(UDPLayer, self).__init__(payload, header, verbose)
@@ -135,9 +157,36 @@ class UDPLayer(TransportLayer):
                                                self.length,
                                                self.payload)
 
+    # ----- Public Methods ----- #
     def getLength(self):
+        """ Getter method for length attribute. """
         return self.length
 
+    def setPayload(self, payload, init=False):
+        """ Override for BaseLayer setPayload. Includes recalculation of  payload length. """
+        self.checkPayload(payload)
+        self.payload = payload
+        if not init:  # Allows length attribute of recieved message to take the place of a calculated message length
+            self.length = len(payload)
+
+        if self.verbose and not init:  # This message is turned off for initilization
+            print("Message set as {} and message length calculated as {}.").format(self.payload, self.length)
+
+    # ----- Private Methods ------ #
+    def checkPayload(self, payload):
+        """ Override of BaseLayer checkPayload. Checks for payload type as string instead of BaseLayer. """
+        if not(isinstance(payload, basestring)):
+            raise ValueError("Message is not a string!")
+
+# ----- Unit Testing ----- #
 if __name__ == "__main__":
     print("Running unit tests for {}").format(__file__)
-    dl = DatalinkLayer("INIIINEEE06TESTMSG", verbose=True)
+
+    print("Attempting bottom up (msg recieved) stack construction...")
+    dl = DatalinkLayer("INIIINEEE06RECVMSG", verbose=True)
+    print(" ")
+
+    print("Attempting top down (msg constructed) stack constructed...")
+    udp = UDPLayer("APPMSG", ("E", "E"), True)
+    ip = IPLayer(udp, ("IN", "II"), "E", True)
+    dl = DatalinkLayer(ip, ("N", "I"), True)
