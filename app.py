8f495f6f029fad072c4f70c89877bcadb6a35c3c
ManickYoj <nickfrancisci@gmail.com>
2014-05-03 19:47:23 -0400

    syncing

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 #TODO OPCODES! YAY!
 
 import morrowsocket as ms
+import socket as s
 
 
 class App(object):
 
-    def __init__(self, app_id, send_queue):
-        self.socket = ms.MorrowSocket(port=app_id, send_queue=send_queue)
+    def __init__(self, app_id=None, send_queue=None):
+        self.sock_type = None
+
+        if app_id and send_queue:
+            self.socket = ms.MorrowSocket(port=app_id, send_queue=send_queue)
+            self.sock_type = 'morse'
+        else:
+            self.socket = s.socket(2, 2)
+            self.sock_type = 'std'
 
     def pushRecvdMsg(self, msg):
-        self.socket.putmsg(msg)
+        if self.sock_type == 'morse':
+            self.socket.putmsg(msg)
